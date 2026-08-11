3238d9de427ca2f399090d8e303dc7027334c2bc
sammysar078 <77676249+sammysar078@users.noreply.github.com>
2026-08-11 15:21:10 +0530

    Update __init__.py

--- app/bot/handlers/__init__.py
+++ app/bot/handlers/__init__.py
@@ -1,8 +1,10 @@
 from aiogram import Router
 
 from .start import router as start_router
+from .group import router as group_router
 from .chat import router as chat_router
 
 router = Router()
 router.include_router(start_router)
+router.include_router(group_router)
 router.include_router(chat_router)
