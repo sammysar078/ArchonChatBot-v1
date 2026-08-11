67d88338eea17479bfc06b70a9832c21d1948e10
sammysar078 <77676249+sammysar078@users.noreply.github.com>
2026-08-11 15:47:52 +0530

    Update __init__.py

--- app/bot/handlers/__init__.py
+++ app/bot/handlers/__init__.py
@@ -5,6 +5,7 @@ from .memory import router as memory_router
 from .admin import router as admin_router
 from .sudo import router as sudo_router
 from .maintenance import router as maintenance_router
+from .broadcast import router as broadcast_router
 from .group import router as group_router
 from .chat import router as chat_router
 from .events import router as events_router
@@ -15,6 +16,7 @@ router.include_router(memory_router)
 router.include_router(admin_router)
 router.include_router(sudo_router)
 router.include_router(maintenance_router)
+router.include_router(broadcast_router)
 router.include_router(group_router)
 router.include_router(chat_router)
 router.include_router(events_router)
