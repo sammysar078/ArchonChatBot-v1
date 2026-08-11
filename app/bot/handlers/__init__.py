7c91455efaa5e795aa91156b8f868c917d48d2c0
sammysar078 <77676249+sammysar078@users.noreply.github.com>
2026-08-11 15:44:23 +0530

    Update __init__.py

--- app/bot/handlers/__init__.py
+++ app/bot/handlers/__init__.py
@@ -7,6 +7,7 @@ from .sudo import router as sudo_router
 from .maintenance import router as maintenance_router
 from .group import router as group_router
 from .chat import router as chat_router
+from .events import router as events_router
 
 router = Router()
 router.include_router(start_router)
@@ -16,3 +17,4 @@ router.include_router(sudo_router)
 router.include_router(maintenance_router)
 router.include_router(group_router)
 router.include_router(chat_router)
+router.include_router(events_router)
